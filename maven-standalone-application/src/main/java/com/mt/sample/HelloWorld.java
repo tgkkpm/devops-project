52c46335436c7904774f71c5472d1aaec559122f
mirujit <72849445+mirujit@users.noreply.github.com>
2021-11-16 16:14:17 +0530

    Add files via upload

---
+++ maven-standalone-application/src/main/java/com/mt/sample/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.mt.sample;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Techtragna !! Today Date is:  " + getLocalCurrentDate());
+
+	}
+
+	private static Date getLocalCurrentDate() {
+		String m = "Techpragna";
+		return new Date();
+
+	}
+
+}
